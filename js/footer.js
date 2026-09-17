8782763391bed5bca5598eab943c041867d0c187
TitoKilonzo <titokilonzo3@gmail.com>
2026-09-17 09:47:51 +0300

    feat: update contact page with new Google Maps embed and link, enhance footer social media icons with SVGs

--- js/footer.js
+++ js/footer.js
@@ -9,10 +9,18 @@ document.write(`
         </div>
         <p style="color:#cbb595;font-size:.92rem;max-width:32ch">A community-founded foundation in Mwingi, Kenya, supporting needy and vulnerable children in education since 2015.</p>
         <div class="social-row">
-          <a href="https://www.facebook.com/profile.php?id=61577012564904" target="_blank" rel="noopener" aria-label="Facebook">f</a>
-          <a href="https://x.com/UkasiHeart28017" target="_blank" rel="noopener" aria-label="X (Twitter)">X</a>
-          <a href="https://instagram.com/" target="_blank" rel="noopener" aria-label="Instagram">IG</a>
-          <a href="https://tiktok.com/" target="_blank" rel="noopener" aria-label="TikTok">TT</a>
+          <a href="https://www.facebook.com/profile.php?id=61577012564904" target="_blank" rel="noopener" aria-label="Facebook" title="Facebook">
+            <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M14 8h3V4h-3c-3.3 0-5 1.9-5 5v3H6v4h3v4h4v-4h3.2l.8-4H13V9c0-.7.3-1 1-1Z"/></svg>
+          </a>
+          <a href="https://x.com/UkasiHeart28017" target="_blank" rel="noopener" aria-label="X (Twitter)" title="X (Twitter)">
+            <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M18.9 2H22l-6.8 7.8L23.2 22h-6.3l-4.9-6.4L6.4 22H3.3l7.3-8.4L2.2 2h6.4l4.4 5.8L18.9 2Zm-1.1 17.8h1.7L7.6 4.1H5.8l12 15.7Z"/></svg>
+          </a>
+          <a href="https://instagram.com/" target="_blank" rel="noopener" aria-label="Instagram" title="Instagram">
+            <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9a5.5 5.5 0 0 1-5.5 5.5h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm0 2A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 16.5 4h-9ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm5.25-3.25a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5Z"/></svg>
+          </a>
+          <a href="https://tiktok.com/" target="_blank" rel="noopener" aria-label="TikTok" title="TikTok">
+            <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M15.5 2h3.1c.3 1.8 1.3 3.2 3.4 3.7v3.2a8.6 8.6 0 0 1-3.4-1V15a7 7 0 1 1-6.1-6.9v3.3a3.7 3.7 0 1 0 2.9 3.6V2Z"/></svg>
+          </a>
         </div>
       </div>
       <div>
